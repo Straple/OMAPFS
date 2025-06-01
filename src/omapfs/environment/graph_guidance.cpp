a6d60211f18738cfb0d60329f9c382d14c07c368
Straple <hadayegor@gmail.com>
2025-06-01 21:06:58 +0300

    add GraphGuidance

---
+++ src/omapfs/environment/graph_guidance.cpp
@@ -0,0 +1,96 @@
+#include <omapfs/environment/graph_guidance.hpp>
+
+#include <omapfs/basic/assert.hpp>
+#include <omapfs/environment/position.hpp>
+
+GraphGuidance::GraphGuidance(uint32_t rows, uint32_t cols) : rows(rows), cols(cols), graph(rows * cols + 1) {
+}
+
+uint32_t GraphGuidance::get(const Position &p, uint32_t action) const {
+    ASSERT(p.get_pos() < graph.size(), "invalid pos");
+    ASSERT(action < 4, "invalid action");
+#ifdef ENABLE_ROTATE_MODEL
+    ASSERT(p.get_dir() < 4, "invalid dir");
+    return graph[p.get_pos()][p.get_dir()][action];
+#else
+    return graph[p.get_pos()][action];
+#endif
+}
+
+void GraphGuidance::set(const Position &p, uint32_t action, uint32_t weight) {
+    ASSERT(p.get_pos() < graph.size(), "invalid pos");
+    ASSERT(action < 4, "invalid action");
+#ifdef ENABLE_ROTATE_MODEL
+    ASSERT(p.get_dir() < 4, "invalid dir");
+    graph[p.get_pos()][p.get_dir()][action] = weight;
+#else
+    graph[p.get_pos()][action] = weight;
+#endif
+}
+
+std::istream &operator>>(std::istream &input, GraphGuidance &gg) {
+    uint32_t rows, cols;
+    input >> rows >> cols;
+    gg = GraphGuidance(rows, cols);
+#ifdef ENABLE_ROTATE_MODEL
+    for (uint32_t dir = 0; dir <= static_cast<uint32_t>(DirectionType::NORTH); dir++) {
+        for (uint32_t action = 0; action <= static_cast<uint32_t>(ActionType::WAIT); action++) {
+            for (uint32_t pos = 1; pos < gg.graph.size(); pos++) {
+                input >> gg.graph[pos][dir][action];
+            }
+        }
+    }
+#else
+    for (uint32_t action = 0; action <= static_cast<uint32_t>(ActionType::WAIT); action++) {
+        for (uint32_t pos = 1; pos < gg.graph.size(); pos++) {
+            input >> gg.graph[pos][action];
+        }
+    }
+#endif
+    return input;
+}
+
+uint32_t GraphGuidance::get_size() const {
+    return graph.size();
+}
+
+uint32_t GraphGuidance::get_rows() const {
+    return rows;
+}
+
+uint32_t GraphGuidance::get_cols() const {
+    return cols;
+}
+
+std::ostream &operator<<(std::ostream &output, const GraphGuidance &gg) {
+    output << gg.rows << ' ' << gg.cols << '\n';
+#ifdef ENABLE_ROTATE_MODEL
+    for (uint32_t dir = 0; dir < 4; dir++) {
+        for (uint32_t action = 0; action < 4; action++) {
+            for (uint32_t pos = 1; pos < gg.graph.size(); pos++) {
+                if (pos != 1) {
+                    output << ' ';
+                }
+                output << gg.graph[pos][dir][action];
+            }
+            output << '\n';
+        }
+    }
+#else
+    for (uint32_t action = 0; action < 4; action++) {
+        for (uint32_t pos = 1; pos < gg.graph.size(); pos++) {
+            if (pos != 1) {
+                output << ' ';
+            }
+            output << gg.graph[pos][action];
+        }
+        output << '\n';
+    }
+#endif
+    return output;
+}
+
+GraphGuidance &get_gg() {
+    static GraphGuidance gg;
+    return gg;
+}
