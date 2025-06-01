01d4a93a3682f40ceec7734c077073cf6543d913
Straple <hadayegor@gmail.com>
2025-06-01 20:19:24 +0300

    add Map

---
+++ src/omapfs/environment/map.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+#include <iostream>
+
+// contains information about map:
+// rows, cols, is_free
+class Map {
+    uint32_t rows = 0;
+    uint32_t cols = 0;
+
+    std::vector<bool> map;
+
+public:
+    Map() = default;
+
+    [[nodiscard]] uint32_t get_rows() const;
+
+    [[nodiscard]] uint32_t get_cols() const;
+
+    [[nodiscard]] uint32_t get_size() const;
+
+    [[nodiscard]] bool is_free(uint32_t pos) const;
+
+    friend std::istream &operator>>(std::istream &input, Map &map);
+};
+
+Map &get_map();
