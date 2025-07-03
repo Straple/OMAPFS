6efc3fd38417b6d5fd57d9350172ed06c9b50694
Straple <hadayegor@gmail.com>
2025-07-03 09:51:19 +0300

    add GreedyScheduler

---
+++ src/omapfs/scheduler/greedy/greedy_scheduler.cpp
@@ -0,0 +1,248 @@
+#include <omapfs/scheduler/greedy/greedy_scheduler.hpp>
+
+#include <omapfs/environment/info.hpp>
+#include <omapfs/environment/task.hpp>
+#include <omapfs/environment/robot.hpp>
+#include <omapfs/environment/heuristic_matrix.hpp>
+#include <omapfs/basic/assert.hpp>
+#include <omapfs/basic/time.hpp>
+#include <omapfs/basic/tools.hpp>
+
+//#include <atomic>
+#include <unordered_set>
+#include <queue>
+
+void GreedyScheduler::rebuild_dp(uint32_t r) {
+    dp[r].clear();
+    for (uint32_t t: free_tasks) {
+        dp[r].emplace_back(get_dist(r, t), t);
+    }
+    std::sort(dp[r].begin(), dp[r].end());
+    timestep_updated[r] = get_curr_timestep() + 1;
+}
+
+void GreedyScheduler::rebuild_dp(TimePoint end_time) {
+    Timer timer;
+    std::vector<uint32_t> order = free_robots;
+    std::stable_sort(order.begin(), order.end(), [&](uint32_t lhs, uint32_t rhs) {
+        return timestep_updated[lhs] < timestep_updated[rhs];
+    });
+
+    std::atomic<uint32_t> counter{};
+    launch_threads(THREADS_NUM, [&](uint32_t thr) {
+        for (uint32_t i = thr; i < order.size() && get_now() < end_time; i += THREADS_NUM) {
+            rebuild_dp(order[i]);
+            ++counter;
+        }
+    });
+}
+
+uint64_t GreedyScheduler::get_dist(uint32_t r, uint32_t t) const {
+    if (t == -1) {
+        return 1e6;
+    }
+
+    uint32_t source = get_robots()[r].node;
+    uint64_t dist_to_target = get_hm().get(source, task_target[t]);
+    uint64_t dist = dist_to_target * 5 + task_metric[t];
+    ASSERT(static_cast<uint32_t>(dist) == dist, "overflow");
+    return dist;
+}
+
+void GreedyScheduler::remove(uint32_t r) {
+    ASSERT(0 <= r && r < desires.size(), "invalid r");
+    uint32_t t = desires[r];
+    if (t == -1) {
+        return;
+    }
+
+    cur_score -= get_dist(r, t);
+    task_to_robot[t] = -1;
+    desires[r] = -1;
+}
+
+void GreedyScheduler::add(uint32_t r, uint32_t t) {
+    ASSERT(0 <= r && r < desires.size(), "invalid r");
+    ASSERT(0 <= t && t < task_to_robot.size(), "invalid t");
+    ASSERT(desires[r] == -1, "already have task");
+    ASSERT(task_to_robot[t] == -1, "already have robot");
+
+    cur_score += get_dist(r, t);
+    task_to_robot[t] = r;
+    desires[r] = t;
+}
+
+void GreedyScheduler::validate() {
+    /*std::set<uint32_t> S;
+    for (uint32_t r = 0; r < desires.size(); r++) {
+        if (desires[r] != -1) {
+            ASSERT(!S.count(desires[r]), "already contains");
+            S.insert(desires[r]);
+            ASSERT(task_to_robot[desires[r]] == r, "invalid task to robot");
+        }
+    }*/
+}
+
+void GreedyScheduler::update() {
+    desires.resize(get_robots().size(), -1);
+    timestep_updated.resize(desires.size());
+    dp.resize(desires.size());
+
+    free_robots.clear();
+    free_tasks.clear();
+
+    // build free_tasks
+    for (auto &[t, task]: get_task_pool()) {
+        uint32_t r = task.agent_assigned;
+        if (
+                r == -1// нет агента
+#ifdef ENABLE_SCHEDULER_CHANGE_TASK
+            || task.idx_next_loc == 0// мы можем поменять задачу
+#endif
+                ) {
+#ifdef ENABLE_SCHEDULER_CHANGE_TASK
+            task.agent_assigned = -1;// IMPORTANT! remove task agent assigned
+#endif
+            free_tasks.push_back(t);
+        }
+    }
+
+    // build free_robots
+    for (uint32_t r = 0; r < get_robots().size(); r++) {
+        uint32_t t = get_robots()[r].task_id;
+
+        // есть задача и она в процессе выполнения
+        // не можем ее убрать
+        if (get_task_pool().contains(t) && get_task_pool().at(t).is_taken) {
+            desires[r] = t;
+            continue;
+        }
+        if (
+            // нет задачи
+                !get_task_pool().contains(t)
+#ifdef ENABLE_SCHEDULER_CHANGE_TASK
+            || env->task_pool.at(t).idx_next_loc == 0
+#endif
+                ) {
+            free_robots.push_back(r);
+        }
+    }
+
+    // build task_metric, task_target
+    {
+        Timer timer;
+        for (uint32_t t: free_tasks) {
+            if (task_metric.size() <= t) {
+                task_metric.resize(t + 1, -1);
+                task_target.resize(t + 1);
+            }
+            auto &task = get_task_pool().at(t);
+            task_target[t] = task.targets[0];
+
+            uint32_t d = 0;
+            for (int i = 0; i + 1 < task.targets.size(); i++) {
+                uint32_t source = task.targets[i];
+                uint32_t target = task.targets[i + 1];
+                d += get_hm().get(get_graph().get_node(Position(source, 0)), target);
+            }
+            task_metric[t] = d;
+        }
+    }
+
+    for (uint32_t t: free_tasks) {
+        if (t >= task_to_robot.size()) {
+            task_to_robot.resize(t + 1, -1);
+        }
+        task_to_robot[t] = -1;
+    }
+
+    cur_score = 0;
+    for (uint32_t r: free_robots) {
+        desires[r] = -1;
+        cur_score += get_dist(r, desires[r]);
+    }
+    validate();
+}
+
+void GreedyScheduler::solve(TimePoint end_time) {
+    Timer timer;
+    for (uint32_t r: free_robots) {
+        remove(r);
+    }
+    std::unordered_set<uint32_t> used_task;
+
+    auto validate_task = [&](uint32_t task_id) {
+        // task is already used
+        if (used_task.count(task_id)) {
+            return false;
+        }
+        // this task is not available
+        if (!get_task_pool().contains(task_id)) {
+            return false;
+        }
+        // robot already used this task
+        if (get_task_pool().at(task_id).agent_assigned != -1) {
+            return false;
+        }
+        return true;
+    };
+
+    // (dist, r, index)
+    std::priority_queue<std::tuple<uint32_t, uint32_t, uint32_t>, std::vector<std::tuple<uint32_t, uint32_t, uint32_t>>, std::greater<>> Heap;
+    for (uint32_t r: free_robots) {
+        if (!dp[r].empty()) {
+            Heap.push({dp[r][0].first, r, 0});
+        }
+    }
+
+    while (!Heap.empty() && get_now() < end_time) {
+        auto [dist, r, index] = Heap.top();
+        Heap.pop();
+
+        uint32_t task_id = dp[r][index].second;
+        ASSERT(dist == dp[r][index].first, "invalid dist");
+
+        if (!validate_task(task_id)) {
+            index++;
+
+            if (index < dp[r].size()) {
+                Heap.push({dp[r][index].first, r, index});
+            }
+
+            continue;
+        }
+
+        ASSERT(get_task_pool().contains(task_id), "no contains");
+        ASSERT(get_task_pool().at(task_id).agent_assigned == -1, "already assigned");
+        ASSERT(!used_task.count(task_id), "already used");
+
+        add(r, task_id);
+        used_task.insert(task_id);
+    }
+
+    validate();
+
+    // если робот без задачи, то мы ее дадим. это нужно для WPPL, который не может без цели, он не самурай
+    auto it = free_tasks.begin();
+    for (uint32_t r = 0; r < desires.size(); r++) {
+        if (desires[r] == -1) {
+            while (it != free_tasks.end() && task_to_robot[*it] != -1) {
+                it++;
+            }
+            ASSERT(it != free_tasks.end(), "unable to set task");
+            add(r, *it);
+        }
+    }
+}
+
+std::vector<int> GreedyScheduler::get_schedule() const {
+    std::vector<int> result(desires.size());
+    for (uint32_t r = 0; r < desires.size(); r++) {
+        result[r] = static_cast<int>(desires[r]);
+    }
+    return result;
+}
+
+double GreedyScheduler::get_score() const {
+    return cur_score;
+}
---
+++ src/omapfs/scheduler/greedy/greedy_scheduler.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <omapfs/basic/time.hpp>
+
+#include <cstdint>
+#include <vector>
+
+#include <settings.hpp>
+
+struct GreedyScheduler {
+
+    double cur_score = 0;
+
+    // desires[r] = task id
+    std::vector<uint32_t> desires;
+
+    // task_to_robot[task] = robot id
+    std::vector<uint32_t> task_to_robot;
+
+    std::vector<uint32_t> free_robots;
+
+    std::vector<uint32_t> free_tasks;
+
+    // dp[r] = отсортированный вектор (dist, task_id)
+    std::vector<std::vector<std::pair<uint32_t, uint32_t>>> dp;
+
+    std::vector<uint32_t> timestep_updated;
+
+    // task_metric[t]
+    std::vector<uint32_t> task_metric;
+
+    // task_target[t] = цель задачи (pos)
+    std::vector<uint32_t> task_target;
+
+    void rebuild_dp(uint32_t r);
+
+    [[nodiscard]] uint64_t get_dist(uint32_t r, uint32_t t) const;
+
+    void remove(uint32_t r);
+
+    void add(uint32_t r, uint32_t t);
+
+    void validate();
+
+public:
+    GreedyScheduler() = default;
+
+    void update();
+
+    void rebuild_dp(TimePoint end_time);
+
+    void solve(TimePoint end_time);
+
+    [[nodiscard]] std::vector<int> get_schedule() const;
+
+    [[nodiscard]] double get_score() const;
+};
