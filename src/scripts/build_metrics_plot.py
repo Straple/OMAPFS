a3eb676c866695c1a59330a2cd4f6d4fd40c0cc9
Straple <hadayegor@gmail.com>
2025-07-16 12:53:09 +0300

    update script

--- src/scripts/build_metrics_plot.py
+++ src/scripts/build_metrics_plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 import matplotlib.colors
-#import seaborn as sns
+# import seaborn as sns
 import pandas as pd
 from PIL import Image
 from matplotlib.ticker import FixedFormatter
@@ -14,22 +14,30 @@ from matplotlib.ticker import MaxNLocator
 # plan_algos_name = ['EPIBT+LNS+GG', 'EPIBT+LNS', 'EPIBT+GG', 'EPIBT', 'PIBT+GG', 'PIBT', 'WPPL+GG', 'PIBT+traffic flow']
 # markers = ['o', 'v', 's', 'p', '*', 'x', 'D', 'P', 'o', 'v', 's', 'p', '*', 'x', 'D', 'P', 'o', 'v', 's', 'p', '*', 'x', 'D', 'P']
 
-#color_palette = sns.color_palette("tab10", 8)
-#plt.rcParams['axes.prop_cycle'] = plt.cycler(color=color_palette)
+# color_palette = sns.color_palette("tab10", 8)
+# plt.rcParams['axes.prop_cycle'] = plt.cycler(color=color_palette)
 
 data = pd.read_csv('metrics.csv', sep=',')
 data['avg step time (ms)'] = data['avg step time (ms)'].astype(int)
+
+maps = set(data.groupby('map type').groups)
+print("maps:", maps)
 
 is_first = True
 
 
 def add_map(map_name, map_text, column):
     global is_first
+    if not map_name in maps:
+        return
     grouped = data.groupby('map type').get_group(map_name)
     grouped = grouped.groupby('planner type')
 
     if True:
-        ax = axes[0][column]
+        if len(maps) == 1:
+            ax = axes[0]
+        else:
+            ax = axes[0][column]
         ax.imshow(np.asarray(Image.open("image/" + map_name.lower() + '.png')))
         ax.title.set_text(map_text)
         ax.set_xticks([])
@@ -39,7 +47,10 @@ def add_map(map_name, map_text, column):
         df = grouped.get_group(planner_type)
 
         if True:
-            ax = axes[1][column]
+            if len(maps) == 1:
+                ax = axes[1]
+            else:
+                ax = axes[1][column]
             ax.plot(df['agents num'], df['throughput'], alpha=1, label=planner_type)
             if is_first:
                 ax.set_ylabel('Throughput')
@@ -48,7 +59,10 @@ def add_map(map_name, map_text, column):
             ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 
         if True:
-            ax = axes[2][column]
+            if len(maps) == 1:
+                ax = axes[2]
+            else:
+                ax = axes[2][column]
             ax.plot(df['agents num'], df['avg step time (ms)'], alpha=1, label=planner_type)
             ax.set_yscale('log')
             if is_first:
@@ -62,7 +76,7 @@ def add_map(map_name, map_text, column):
 
 
 if __name__ == '__main__':
-    fig, axes = plt.subplots(3, 5, figsize=(14, 10))
+    fig, axes = plt.subplots(3, len(maps), figsize=(14, 10))
 
     add_map('RANDOM', 'random-32-32-20\nSize: 32x32\n|V|=819', 0)
     add_map('CITY', 'Paris-1-256\nSize: 256x256\n|V|=47240', 1)
