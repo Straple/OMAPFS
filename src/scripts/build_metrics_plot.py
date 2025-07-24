d4cfc0d47e95c0732d0d09b3aaf1d9b3c7f7d524
Straple <hadayegor@gmail.com>
2025-07-24 20:41:48 +0300

    update build_metrics_plot.py

--- src/scripts/build_metrics_plot.py
+++ src/scripts/build_metrics_plot.py
@@ -8,20 +8,31 @@ from PIL import Image
 from matplotlib.ticker import FixedFormatter
 from matplotlib.ticker import MaxNLocator
 
-PLANNERS = [#'EPIBT_LNS(3)', 'EPIBT_old(3)+mll+lns',
-            #'EPIBT_old(3)+mll+lns',
-            #'EPIBT(4)', 'EPIBT_old(4)',
-            #'EPIBT(3)', 'EPIBT(4)',
-            #'EPIBT(3)+rollback+mll',
-            #'EPIBT(4)+rollback+mll',
+PLANNERS = [
 
-            #'EPIBT_LNS(3)', 'EPIBT_LNS(3)-temperature'
+    # 'PIBT',
+    # 'PIBT-C', # PIBT + пункт C про перепосещение но с ограничением в 10 на одного агента
 
-            #'EPIBT(3)', 'EPIBT_old(3)',
-            #'CAUSAL_PIBT', 'PIBT_TF',
+    # 'Causal PIBT',
+    # 'PIBT+traffic flow',
 
-            #'PIBT', 'PIBT+revisit',
-            ]
+    # 'EPIBT(1)',
+    # 'EPIBT(2)',
+    # 'EPIBT(3)',
+    # 'EPIBT(4)',
+    # 'EPIBT(5)',
+
+    # 'LoRR24-Winner',
+    'LoRR24-Winner+GG',
+
+    # 'WPPL',
+    'WPPL+GG',
+
+    # 'EPIBT(3)+LNS',
+    'EPIBT(3)+LNS+GG',
+    # 'EPIBT(4)+LNS+GG',
+
+]
 
 # plan_algos = ["EPIBT(1)", "EPIBT(2)", "EPIBT(3)"]
 # colors = ['lime', 'dodgerblue', 'orange', 'red', 'blueviolet', 'aqua', 'deeppink', 'brown']
@@ -33,7 +44,6 @@ markers = ['o', 'v', 's', 'p', '*', 'x', 'D', 'P']
 # plt.rcParams['axes.prop_cycle'] = plt.cycler(color=color_palette)
 
 data = pd.read_csv('metrics.csv', sep=',')
-#data['avg step time (ms)'] = data['avg step time (ms)'].astype(int)
 
 maps = set(data.groupby('map type').groups)
 print("maps:", maps)
@@ -42,6 +52,7 @@ is_first = True
 
 planner_to_marker = dict()
 marker_it = 0
+
 
 def add_map(map_name, map_text, column):
     global marker_it
@@ -76,10 +87,10 @@ def add_map(map_name, map_text, column):
                 ax = axes[1]
             else:
                 ax = axes[1][column]
-            ax.plot(df['agents num'], df['throughput'], alpha=1, label=planner_type, marker=planner_to_marker[planner_type])
+            ax.plot(df['agents num'], df['throughput'], alpha=1, label=planner_type,
+                    marker=planner_to_marker[planner_type])
             if is_first:
                 ax.set_ylabel('Throughput')
-            ax.yaxis.set_major_locator(MaxNLocator(integer=True))
             ax.grid(True)
 
         if True:
@@ -87,7 +98,8 @@ def add_map(map_name, map_text, column):
                 ax = axes[2]
             else:
                 ax = axes[2][column]
-            ax.plot(df['agents num'], df['avg planner time (ms)'], alpha=1, label=planner_type, marker=planner_to_marker[planner_type])
+            ax.plot(df['agents num'], df['avg planner time (ms)'], alpha=1, label=planner_type,
+                    marker=planner_to_marker[planner_type])
             ax.set_yscale('log')
             if is_first:
                 ax.set_ylabel('Decision Time (ms)')
@@ -98,9 +110,16 @@ def add_map(map_name, map_text, column):
     if is_first:
         is_first = False
 
+    if len(maps) == 1:
+        ax = axes[1]
+    else:
+        ax = axes[1][column]
+    ax.set_ylim(0, None)
+    ax.set_yticks(np.unique(np.round(ax.get_yticks()).astype(int)))
+
 
 if __name__ == '__main__':
-    fig, axes = plt.subplots(3, len(maps), figsize=(14, 12))
+    fig, axes = plt.subplots(3, len(maps), figsize=(14, 8))
 
     add_map('RANDOM', 'random-32-32-20\nSize: 32x32\n|V|=819', 0)
     add_map('CITY', 'Paris-1-256\nSize: 256x256\n|V|=47240', 1)
@@ -124,5 +143,5 @@ if __name__ == '__main__':
     # print(labels)
     fig.legend(lines, labels, loc='lower center', ncol=6)
 
-    plt.savefig("metrics_plot.pdf", format='pdf', dpi=800)
-    #plt.show()
+    plt.savefig("metrics_plot.pdf", format='pdf', bbox_inches="tight", dpi=800, pad_inches=0.1)
+    # plt.show()
